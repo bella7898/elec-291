68db7d44a5fcabd76029becc90ef5fdfaf0dff0f
belw7898 <bella.wang.10@gmail.com>
2026-03-12 14:30:39 -0700

    project2

---
+++ Project2/Project.mk
@@ -0,0 +1,35 @@
+SHELL=cmd
+OBJS=usart.o project.o lcd.o
+PORTN=$(shell type COMPORT.inc)
+
+avr_printf.elf: $(OBJS)
+	avr-gcc -mmcu=atmega328p $(OBJS) -o project.elf
+	avr-objcopy -j .text -j .data -O ihex project.elf project.hex
+	@echo done!
+
+usart.o: usart.c usart.h
+	avr-gcc -g -Os -Wall -mmcu=atmega328p -c usart.c
+
+lcd.o: lcd.c lcd.h
+	avr-gcc -g -Os -Wall -mmcu=atmega328p -c lcd.c
+
+project.o: project.c usart.h project.h
+	avr-gcc -g -Os -Wall -mmcu=atmega328p -c project.c
+
+clean:
+	@del *.hex *.elf *.o 2>nul
+
+FlashLoad:
+	@taskkill /f /im putty.exe /t /fi "status eq running" > NUL
+	spi_atmega -p -v -crystal project.hex
+	@cmd /c start putty.exe -serial $(PORTN) -sercfg 115200,8,n,1,N
+
+putty:
+	@taskkill /f /im putty.exe /t /fi "status eq running" > NUL
+	@cmd /c start putty.exe -serial $(PORTN) -sercfg 115200,8,n,1,N
+
+dummy: avr_printf.hex
+	@echo Hello dummy!
+
+explorer:
+	cmd /c start explorer .
